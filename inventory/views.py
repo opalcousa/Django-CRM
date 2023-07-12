4ae2e146204d9dfccb73304a96d3ab7487216b44
Drake Stapleton <opalcousa@gmail.com>
2023-07-12 17:34:11 -0500

    Added inventory views and URLs for creating, listing, and managing products, stocks, suppliers, purchase orders, and sales orders.

--- inventory/views.py
+++ inventory/views.py
@@ -1,1 +1,49 @@
-# File is now empty
+from django.shortcuts import render
+from django.views import generic
+from .models import Product, Stock, Supplier, PurchaseOrder, SalesOrder
+from .forms import ProductForm, StockForm, SupplierForm, PurchaseOrderForm, SalesOrderForm
+
+class ProductListView(generic.ListView):
+    model = Product
+
+class ProductCreateView(generic.edit.CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'inventory/product_form.html'
+    success_url = '/inventory/products/'
+
+class StockListView(generic.ListView):
+    model = Stock
+
+class StockCreateView(generic.edit.CreateView):
+    model = Stock
+    form_class = StockForm
+    template_name = 'inventory/stock_form.html'
+    success_url = '/inventory/stocks/'
+
+class SupplierListView(generic.ListView):
+    model = Supplier
+
+class SupplierCreateView(generic.edit.CreateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'inventory/supplier_form.html'
+    success_url = '/inventory/suppliers/'
+
+class PurchaseOrderListView(generic.ListView):
+    model = PurchaseOrder
+
+class PurchaseOrderCreateView(generic.edit.CreateView):
+    model = PurchaseOrder
+    form_class = PurchaseOrderForm
+    template_name = 'inventory/purchase_order_form.html'
+    success_url = '/inventory/purchase_orders/'
+
+class SalesOrderListView(generic.ListView):
+    model = SalesOrder
+
+class SalesOrderCreateView(generic.edit.CreateView):
+    model = SalesOrder
+    form_class = SalesOrderForm
+    template_name = 'inventory/sales_order_form.html'
+    success_url = '/inventory/sales_orders/'
