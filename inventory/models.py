6cbd7ee81f001db601f5b99eb7c4ea733fe0cc3a
Drake Stapleton <opalcousa@gmail.com>
2023-07-12 17:58:57 -0500

    aider: Add __str__ method to Product model to return the product's name as a string representation.
    # Context:
    USER: In the stock menu, it's showing: Product:
    Product object (1) instead of the actual product name
    ASSISTANT: To fix this, you need to modify the `__str__` method in the `Product` model in the `models.py` file. This method should return a string representation of the object, in this case, the product's name.
    
    Here's how you can do it:
    
    ```python
    inventory/models.py
    <<<<<<< ORIGINAL
    class Product(models.Model):
        name = models.CharField(max_length=200)
        description = models.TextField()
    =======
    class Product(models.Model):
        name = models.CharField(max_length=200)
        description = models.TextField()
    
        def __str__(self):
            return self.name
    >>>>>>> UPDATED
    ```
    
    This change will make Django use the product's name when it needs to convert the product to a string. For example, in the admin site or any other place where Django uses a string representation of the product.

--- inventory/models.py
+++ inventory/models.py
@@ -7,6 +7,9 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Stock(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
