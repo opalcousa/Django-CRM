ddc1057144854bde6bd3b7729fd386ff28e1b1cf
Drake Stapleton <opalcousa@gmail.com>
2023-07-12 18:25:05 -0500

    aider: Modified the `__str__` method in the `Supplier` model to display the name of the supplier object.
    # Context:
    USER: I'm seeing this on my add purchase order form: Supplier:
    Supplier object (1)
    
    ASSISTANT: It seems like the form is displaying the default string representation of the Supplier object, which is not very informative. To fix this, you need to modify the `__str__` method in the `Supplier` model to return a more descriptive string.
    
    Here's how you can do it:
    
    ```python
    inventory/models.py
    <<<<<<< ORIGINAL
    class Supplier(models.Model):
        name = models.CharField(max_length=200)
        contact_info = models.TextField()
    =======
    class Supplier(models.Model):
        name = models.CharField(max_length=200)
        contact_info = models.TextField()
    
        def __str__(self):
            return self.name
    >>>>>>> UPDATED
    ```
    
    This change will make the `Supplier` object display its `name` instead of the default object representation.

--- inventory/models.py
+++ inventory/models.py
@@ -18,6 +18,9 @@ class Supplier(models.Model):
     name = models.CharField(max_length=200)
     contact_info = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class PurchaseOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
