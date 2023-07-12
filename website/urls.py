c7be0a4720f6321f3ed1d04b17f3a805fa8e37cd
Drake Stapleton <opalcousa@gmail.com>
2023-07-12 18:03:49 -0500

    Refactor website/urls.py to remove unnecessary import and reorganize urlpatterns.

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    # existing paths...
-    path('inventory/', include('inventory.urls')),
-]
     path('', views.home, name='home'),
     #path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
@@ -13,5 +11,5 @@ urlpatterns = [
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
-
+    path('inventory/', include('inventory.urls')),
 ]
