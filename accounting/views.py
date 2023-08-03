6d2fac9f335a58d4eb0b8dff46fe92235b6c7eeb
Drake Stapleton <opalcousa@gmail.com>
2023-08-03 08:44:28 -0500

    aider: Added 'orders' view to accounting/views.py.
    # Context:
    USER:   File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
      File "/Users/drakestapleton/PycharmProjects/Django-CRM/accounting/urls.py", line 8, in <module>
        path('orders/', views.orders, name='orders'),
                        ^^^^^^^^^^^^
    AttributeError: module 'accounting.views' has no attribute 'orders'. Did you mean: 'Order'?

--- accounting/views.py
+++ accounting/views.py
@@ -5,5 +5,24 @@ def dashboard(request):
     orders = Order.objects.all()
     return render(request, 'dashboard.html', {'orders': orders})
 
-def index_view(request):
-    return render(request, 'index.html')
+def orders(request):
+    orders = Order.objects.all()
+    return render(request, 'orders.html', {'orders': orders})
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
+
+def order_detail(request, order_id):
+    order = Order.objects.get(id=order_id)
+    return render(request, 'order_detail.html', {'order': order})
+
+def profit_loss(request):
+    total_sales = 1000  # Replace with actual calculation
+    total_purchase_orders = 500  # Replace with actual calculation
+    profit_loss = total_sales - total_purchase_orders  # Replace with actual calculation
+    return render(request, 'profit_loss.html', {'total_sales': total_sales, 'total_purchase_orders': total_purchase_orders, 'profit_loss': profit_loss})
