05db145450c8d99e0e16d6a842a7c377c5e3b5b8
Drake Stapleton <opalcousa@gmail.com>
2023-08-03 07:37:53 -0500

    Added accounting URLs for index view, dashboard, orders, order detail, products, product detail, and profit loss.

---
+++ accounting/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounting'
+urlpatterns = [
+    path('', views.index_view, name='index_view'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('orders/', views.orders, name='orders'),
+    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('products/', views.products, name='products'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('profit_loss/', views.profit_loss, name='profit_loss'),
+]
