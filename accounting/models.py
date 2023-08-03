6651adf05232b7d06f06b4cdc7dde63ef5d58da6
Drake Stapleton <opalcousa@gmail.com>
2023-08-03 08:13:04 -0500

    Removed the Product model and its fields from the accounting models.

--- accounting/models.py
+++ accounting/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self):
-        return self.name
