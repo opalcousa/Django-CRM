44d65cc4e20a145f62b031b8abdfa2bcec7fbb15
Drake Stapleton <opalcousa@gmail.com>
2023-08-03 08:45:00 -0500

    aider: Removed the index_view path and added the dashboard path in accounting/urls.py.
    # Context:
    USER:   File "/Users/drakestapleton/PycharmProjects/Django-CRM/accounting/urls.py", line 6, in <module>
        path('', views.index_view, name='index_view'),
                 ^^^^^^^^^^^^^^^^
    AttributeError: module 'accounting.views' has no attribute 'index_view'

--- accounting/urls.py
+++ accounting/urls.py
@@ -3,13 +3,10 @@ from . import views
 
 app_name = 'accounting'
 urlpatterns = [
-    path('', views.index_view, name='index_view'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('orders/', views.orders, name='orders'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
     path('products/', views.products, name='products'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('generate_orders/', views.generate_orders, name='generate_orders'),
-    path('generate_orders/', views.generate_orders, name='generate_orders'),
     path('profit_loss/', views.profit_loss, name='profit_loss'),
 ]
