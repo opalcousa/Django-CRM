3628759389a5705126b79157562208480cb57fbe
Drake Stapleton <opalcousa@gmail.com>
2023-08-03 08:19:50 -0500

    Removed the purchase order generation and saving functionality.

--- accounting/main.py
+++ accounting/main.py
@@ -1,6 +1,5 @@
 import datetime
 from .order_generator import OrderGenerator
-from .purchase_order_generator import PurchaseOrderGenerator
 from .save_records import save_records
 from .profit_loss_statement import main as calculate_profit_loss
 
@@ -11,16 +10,9 @@ def main():
     order_generator = OrderGenerator(start_date, end_date)
     orders = order_generator.generate_orders(100)
 
-    # Generate purchase orders
-    purchase_order_generator = PurchaseOrderGenerator()
-    purchase_orders = purchase_order_generator.generate_purchase_order(orders)
-
     # Save records
     order_fieldnames = ['order_date', 'customer_name', 'product_names', 'total_price']
     save_records(orders, 'orders.csv', order_fieldnames)
-
-    purchase_order_fieldnames = ['serial_number', 'cost_per_unit', 'product_id', 'hemp_oil_gallons', 'aloe_vera_gallons', 'bottles', 'product_base_gallons', 'total_cost']
-    save_records(purchase_orders, 'purchase_orders.csv', purchase_order_fieldnames)
 
     # Calculate profit/loss
     calculate_profit_loss()
