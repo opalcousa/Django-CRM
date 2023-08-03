109df37501be3fa3853a55f5fbd556a5ef1ec5e5
Drake Stapleton <opalcousa@gmail.com>
2023-08-03 12:24:04 -0500

    Revert "aider: Added a new URL pattern for orders and corrected a URL pattern for index."
    This reverts commit 6e0d2861a4cc63e3656c602e7a3729912d1e7ddd.

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'accounting'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('orders/', views.orders, name='orders'),  # Add this line
+    path('orders/', views.orders, name='orders'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
     path('products/', views.products, name='products'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('profit_loss/', views.profit_loss, name='profit_loss'),
-    path('index/', views.index_view, name='index'),
+    path('index/', views.index_view, name='index'),  # Corrected line
     path('generate_orders/', views.generate_orders, name='generate_orders'),
     path('export_orders/', views.export_orders, name='export_orders'),
 ]
